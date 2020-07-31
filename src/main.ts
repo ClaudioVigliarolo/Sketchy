7b9572ef4371f96eb5ed50f5cd5d8e3b70bd6e61
claudio <claviglia53@gmail.com>
2020-07-31 14:37:35 +0200

    Adding files.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import axios from 'axios';
 import { auth } from './firebase/index';
 
 Vue.config.productionTip = false;
